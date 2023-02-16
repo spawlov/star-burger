f6d1c2bcc2ed75efbe60e3b1e21096476603e19c
spawlov <spawlov@gmail.com>
2023-02-16 12:37:08 +0700

    Add data validation for register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -2,7 +2,9 @@ import json
 
 from django.http import JsonResponse
 from django.templatetags.static import static
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Product, Order, ProductOrder
 
@@ -63,20 +65,43 @@ def product_list_api(request):
 def register_order(request):
     if request.method == 'POST':
         order_data = request.data
-
-        order = Order.objects.create(
-            first_name=order_data['firstname'],
-            last_name=order_data['lastname'],
-            phone=order_data['phonenumber'],
-            address=order_data['address']
-        )
-
-        for position in order_data['products']:
-            product = Product.objects.get(pk=position['product'])
-            ProductOrder.objects.create(
-                order=order,
-                product=product,
-                quantity=position['quantity']
+        try:
+            products = order_data['products']
+        except KeyError:
+            return Response(
+                {'error': 'products: Обязательное поле.'},
+                status=status.HTTP_400_BAD_REQUEST
             )
-        print(order_data)
-    return JsonResponse({})
+        else:
+            if products is None:
+                return Response(
+                    {'error': 'products: Это поле не может быть пустым.'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            if not isinstance(products, list):
+                return Response(
+                    {'error': 'products: Ожидался list со значениями'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            if not len(products):
+                return Response(
+                    {'error': 'products: Этот список не может быть пустым.'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            order = Order.objects.create(
+                first_name=order_data['firstname'],
+                last_name=order_data['lastname'],
+                phone=order_data['phonenumber'],
+                address=order_data['address']
+            )
+
+            for position in order_data['products']:
+                product = Product.objects.get(pk=position['product'])
+                ProductOrder.objects.create(
+                    order=order,
+                    product=product,
+                    quantity=position['quantity']
+                )
+
+    return Response({}, status=status.HTTP_201_CREATED)
