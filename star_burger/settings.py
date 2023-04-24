a167812b8771d8dfde23038a000118076ef0c96c
spawlov <spawlov@gmail.com>
2023-04-25 00:03:56 +0700

    Added CSRF_TRUSTED_ORIGINS to settings.py

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -16,6 +16,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', [])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
