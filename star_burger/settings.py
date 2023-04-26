128f7fd3c61fba2da6c6c45273ff552c789380e3
spawlov <spawlov@gmail.com>
2023-04-27 00:33:43 +0700

    Changed settings.py: the commented-out code has been removed

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -92,12 +92,6 @@ WSGI_APPLICATION = 'star_burger.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-#     )
-# }
-
 DATABASES = {
     'default': dj_database_url.config(
         default=env('DB_URL'),
